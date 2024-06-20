3f60fb2e1566f6fda854c15604445172d915badc
Usama Meer <usamameer256@gmail.com>
2024-06-20 23:52:19 +0500

    "Updated GameStoreContext to seed Genre data, modified AssemblyInfo, and updated various binary files and caches."

--- GameStore/Data/GameStoreContext.cs
+++ GameStore/Data/GameStoreContext.cs
@@ -10,6 +10,18 @@ namespace GameStore.Data;
         public DbSet<Game> Games=>Set<Game>();
         public DbSet<Genre> Genres =>Set<Genre>();
 
-        
+
+        //This is going to add the following data into the table
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Genre>().HasData(
+                new {Id=1,Name="Fighting"},
+                new {Id=2,Name="Roleplaying"},
+                new {Id=3, Name="Sports"},
+                new {Id=4, Name="Racing"},
+                new {Id=5, Name="Kids and Family"}
+            );
     }
+
+}
     
